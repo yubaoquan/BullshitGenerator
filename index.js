f6523ce093ea568e45baf648939f1dbb72276016
sirsui <805593597@qq.com>
2019-11-22 14:59:04 +0800

    添加导出

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function   choice(a){
 let arr=data[a];
   return arr[ (Math.random() *arr.length) | 0];
 }
-
-function generator(title, length = 1000) {
+function generator(title="无标题", length = 1000) {
     let body = "";
     while (body.length < length) {
         let num = Math.random() * 100 | 0;
@@ -27,4 +26,5 @@ function generator(title, length = 1000) {
     }
     return body;
 }
-console.log( generator('nodejs牛逼',2000))
+
+export default generator;
